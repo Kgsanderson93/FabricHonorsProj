24c6a17bce5ec0cfd0fdd47cdb6ddcb8f7cd59ef
Kayla <Derek@DESKTOP-BHV4BL1>
2020-04-12 12:44:58 -0400

    clear Fields Works

--- src/Utility.java
+++ src/Utility.java
@@ -11,33 +11,29 @@ import javax.swing.JTextField;
 
 public class Utility {
 
+	public static void clearFields(Container container) {
 
-public static void clearFields(Container container) {
-	
-	
-	 for(Component c : container.getComponents()){
-		 if(c instanceof Container) {
-			Container cont= (Container)c;
-			clearFields(cont);
-		 }
-		 else if(c instanceof JTextField) {
-			JTextField item=(JTextField)c;
-			item.setText("");				
+		for (Component c : container.getComponents()) {
+			if (c instanceof Container) {
+				Container cont = (Container) c;
+				clearFields(cont);
+			}
+			if (c instanceof JTextField) {
+				JTextField item = (JTextField) c;
+				item.setText("");
+			} else if (c instanceof JComboBox) {
+				@SuppressWarnings("rawtypes")
+				JComboBox box = (JComboBox) c;
+				box.setSelectedIndex(0);
+			} else if (c instanceof JRadioButton) {
+				JRadioButton button = (JRadioButton) c;
+				button.setSelected(false);
+			} else if (c instanceof JCheckBox) {
+				JCheckBox box2 = (JCheckBox) c;
+				box2.setSelected(false);
+			}
+
 		}
-		else if(c instanceof JComboBox) {
-			@SuppressWarnings("rawtypes")
-			JComboBox box= (JComboBox)c;
-			box.setSelectedIndex(0);				
-		}
-		else if(c instanceof JRadioButton) {
-			JRadioButton button= (JRadioButton) c;
-			button.setSelected(false);
-		}
-		else if(c instanceof JCheckBox) {
-			JCheckBox box2= (JCheckBox)c;
-			box2.setSelected(false);
-		}
+
 	}
-
-}
 }
